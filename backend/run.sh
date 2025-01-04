0daa54ad44e81847f0c76e71930835742be18469
Mouad Bennani <40070686+GunDalf101@users.noreply.github.com>
2025-01-04 06:39:11 +0100

    matchmaking actually working ig

--- backend/run.sh
+++ backend/run.sh
@@ -1,29 +1,29 @@
 
 
-docker stop postgres-db
-docker rm -f postgres-db
-docker pull postgres:latest
+# docker stop postgres-db
+# docker rm -f postgres-db
+# docker pull postgres:latest
 
-docker container rm postgres-db
-docker run -d \
-  --name postgres-db \
-  -e POSTGRES_USER=myuser \
-  -e POSTGRES_PASSWORD=mypassword \
-  -e POSTGRES_DB=mydb \
-  -p 5432:5432 \
-  postgres:latest
+# docker container rm postgres-db
+# docker run -d \
+#   --name postgres-db \
+#   -e POSTGRES_USER=myuser \
+#   -e POSTGRES_PASSWORD=mypassword \
+#   -e POSTGRES_DB=mydb \
+#   -p 5432:5432 \
+#   postgres:latest
 
-docker pull redis:latest
+# docker pull redis:latest
 
-docker container rm redis-db
-docker run -d \
-  --name redis-db \
-  -p 6379:6379 \
-  redis:latest
+# docker container rm redis-db
+# docker run -d \
+#   --name redis-db \
+#   -p 6379:6379 \
+#   redis:latest
 
 # sleep 10
-python manage.py makemigrations
-python manage.py migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
 
 # export DJANGO_SETTINGS_MODULE=transcendence.settings
 
@@ -33,6 +33,6 @@ python manage.py migrate
 
 echo "!!!!! Run in another terminal: 'python manage.py process_tasks' to receive emails. !!!!!"
 
-python manage.py runserver 8000
+python3 manage.py runserver 8000
 
 
